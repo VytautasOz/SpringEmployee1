44e3d20ac25754c45fda3faef302b459d679ae12
VytautasOz <patrikasv@gmail.com>
2025-03-19 21:40:41 +0200

    Initial commit

---
+++ SpringEmployee1/src/test/java/com/example/SpringEmployee1/SpringEmployee1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringEmployee1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEmployee1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
